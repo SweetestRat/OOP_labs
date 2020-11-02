c2035515b82861d4fdcf44ed1a29f1edbaa1b7bf
Vlada Gilde <61109457+SweetestRat@users.noreply.github.com>
2020-11-02 22:05:46 +0300

    Update Grammar.cs

--- Parser/Grammar.cs
+++ Parser/Grammar.cs
@@ -4,7 +4,7 @@ namespace IniParser
     public class Grammar
     {
         public string section = "^\\[[a-zA-Z_0-9]*\\]$";
-        public string key = "^[a-zA-Z_0-9]*\\=";
+        public string key = "^[a-zA-Z_0-9]*";
         public string valueString = "[a-zA-Z\\.]*$";
         public string valueFloat = "[0-9]*[.,][0-9]*$";
         public string valueInt = "[0-9]*$";
