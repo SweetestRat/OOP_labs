e875761a22ae4927f8daaf43a96d8f43b4db91fb
Vlada Gilde <61109457+SweetestRat@users.noreply.github.com>
2020-12-01 18:42:04 +0300

    Update Shop.cs

--- Shop/Shop.cs
+++ Shop/Shop.cs
@@ -5,32 +5,17 @@ namespace Shop
 {
     public class Shop
     {
-        // List<Product> consignment = new List<Product>();
-
         private string name;
-        public string Name
-        {
-            get => name;
-            set => name = value;
-        }
-        
+        public string Name => name;
+
         private string address;
-        public string Address
-        {
-            get => address;
-            set => address = value;
-        }
+        public string Address => address;
 
         private Guid id;
-        public Guid Id
-        {
-            get => id;
-            set => id = value;
-        }
-        
+        public Guid Id => id;
+
         public Dictionary<Guid, Product> catalog;
 
-        Manager mngr = new Manager();
         public Shop(string _name, string _address)
         { 
             name = _name;
@@ -39,56 +24,43 @@ namespace Shop
             catalog = new Dictionary<Guid, Product>();
         }
 
-        public void AddProduct(Guid _prodid, int _prodPrice, Product _product)
+        public void AddProduct(Product _product, int _prodPrice)
         {
-            if (catalog.ContainsKey(_prodid))
+            if (catalog.ContainsKey(_product.prodId))
             {
                 throw new Exception("Sorry, this prod with unique id is already exist");
             }
-            
-            Product prod = new Product();
-            
-            prod.prodPrice = _prodPrice;
-            prod.prodAmount = 1;
-            prod.prodName = _product.prodName;
-            // product.prodid = _prodid;
-            
-            catalog.Add(_prodid, prod);
+
+            _product.prodPrice = _prodPrice;
+            _product.prodAmount += 1;
+
+            catalog.Add(_product.prodId, _product);
         }
 
         public void AddConsignment(Guid _prodid, int _prodPrice, int _prodAmount, Product _product)
         {
             if (catalog.ContainsKey(_prodid))
             {
-                Product prod = new Product();
-                
-                prod.prodAmount += _prodAmount;
-                prod.prodPrice = _prodPrice;
-                prod.prodName = _product.prodName;
-                catalog[_prodid] = prod;
+                _product.prodAmount += _prodAmount;
+                _product.prodPrice = _prodPrice;
+                catalog[_prodid] = _product;
             }
             else
             {
-                Product prod = new Product();
-                
-                prod.prodAmount += _prodAmount;
-                prod.prodPrice = _prodPrice;
-                prod.prodName = _product.prodName;
-                catalog.Add(_prodid, prod);
+                _product.prodAmount += _prodAmount;
+                _product.prodPrice = _prodPrice;
+                catalog.Add(_prodid, _product);
             }
         }
 
-        public List<KeyValuePair<string, int>> ProdList = new List<KeyValuePair<string, int>>();
-        public List<KeyValuePair<string, int>> ProdListOnSum(int _sum)
+        public void ProdListOnSum(int sum)
         {
-            foreach (var prod in catalog.Values)
+            Console.WriteLine($"\nList of amount of products you can buy in {Name} on {sum}:");
+            foreach (var prod in catalog)
             {
-                int amount = _sum / prod.prodPrice;
-                
-                ProdList.Add(new KeyValuePair<string, int>(prod.prodName, prod.prodPrice));
+                var amount = sum / prod.Value.prodPrice;
+                Console.WriteLine($"{prod.Value.prodName}: {amount} piece(-s) // {prod.Value.prodPrice} x {amount}");
             }
-
-            return ProdList;
         }
     }
 }
