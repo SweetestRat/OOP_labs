3d00847bf0128698dab1d3e04928dfe760b6fbef
Vlada Gilde <61109457+SweetestRat@users.noreply.github.com>
2020-12-01 18:42:26 +0300

    Update Program.cs

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Shop
 {
@@ -7,79 +8,114 @@ namespace Shop
         static void Main(string[] args)
         {
             Manager manager = new Manager();
-            Product product = new Product();
 
             // create 3 shops
-            Shop shop1 = new Shop("Speedo", "Ul. Vosstania, 6");
-            manager.shops.Add(shop1.Id, shop1);
+            Shop Speedo = new Shop("Speedo", "Ul. Vosstania, 6");
+            manager.shops.Add(Speedo.Id, Speedo);
 
-            Shop shop2 = new Shop("Mad Wave", "Bolshaya Pushkarskaya, 54");
-            manager.shops.Add(shop2.Id, shop2);
+            Shop MadWave = new Shop("Mad Wave", "Bolshaya Pushkarskaya, 54");
+            manager.shops.Add(MadWave.Id, MadWave);
             
-            Shop shop3 = new Shop("Arena", "Ul. Marata, 44");
-            manager.shops.Add(shop3.Id, shop3);
+            Shop Arena = new Shop("Arena", "Ul. Marata, 44");
+            manager.shops.Add(Arena.Id, Arena);
 
-            // prod moves)
-            Guid good1 = product.CreateProduct("SportSwimsuit"); //CREATION
+            //create 10 products
+            Product MenSportSwimsuit = new Product("Men Sport Swimsuit"); // 1
+            manager.guids.Add(MenSportSwimsuit.prodId);
+            Product SwimmingTrunks = new Product("Swimming Trunks"); // 2
+            manager.guids.Add(SwimmingTrunks.prodId);
+            Product KidsSwimsuit = new Product("Kids Swimsuit"); // 3
+            manager.guids.Add(KidsSwimsuit.prodId);
+            Product KidsGoggles = new Product("Kids Goggles"); // 4
+            manager.guids.Add(KidsGoggles.prodId);
+            Product CompetitionGoggles = new Product("Competition Goggles"); // 5
+            manager.guids.Add(CompetitionGoggles.prodId);
+            Product WomenSportSwimsuit = new Product("Women Sport Swimsuit"); // 6
+            manager.guids.Add(WomenSportSwimsuit.prodId);
+            Product WomenBeachSwimsuit = new Product("Women Beach Swimsuit"); // 7
+            manager.guids.Add(WomenBeachSwimsuit.prodId);
+            Product AdultPoolSlippers = new Product("Adult Pool Slippers"); // 8
+            manager.guids.Add(AdultPoolSlippers.prodId);
+            Product KidsPoolSlippers = new Product("Kids Pool Slippers"); // 9
+            manager.guids.Add(KidsGoggles.prodId);
+            Product Towel = new Product("Towel"); // 10
+            manager.guids.Add(Towel.prodId);
             
-            shop1.AddProduct(good1, 1400, product); // adding prod to shop
-            shop1.AddConsignment(good1, 1450, 20, product); // adding consignment to shop
-            shop2.AddProduct(good1, 1500, product); // adding prod to shop
-            shop3.AddProduct(good1, 1400, product); // adding prod to shop
-            shop3.AddConsignment(good1, 1450, 50, product); // adding consignment to shop
-
-            Guid good2 = product.CreateProduct("KidsGoggles"); // CREATION
             
-            shop1.AddProduct(good2, 1200, product); // adding prod to shop
-            shop2.AddConsignment(good2, 1900,10, product); // adding consignment to shop
-            shop3.AddConsignment(good2, 1900, 30, product); // adding consignment to shop
-
-            Manager.cheapprodinfo cheapprodinfo = manager.FindCheapestProd("SportSwimsuit"); // finding the cheapest prod + filling in info 'bout it
+            //fill Speedo shop with goods
+            Speedo.AddProduct(MenSportSwimsuit, 2400);
+            Speedo.AddProduct(SwimmingTrunks, 1800);
+            Speedo.AddProduct(CompetitionGoggles, 1500);
+            Speedo.AddProduct(AdultPoolSlippers, 1100);
+            Speedo.AddConsignment(Towel.prodId, 400, 100, Towel);
+            
+            //fill MadWave shop with goods
+            MadWave.AddProduct(MenSportSwimsuit, 2000);
+            MadWave.AddProduct(SwimmingTrunks, 1500);
+            MadWave.AddProduct(KidsSwimsuit, 1200);
+            MadWave.AddProduct(KidsGoggles, 1000);
+            MadWave.AddProduct(CompetitionGoggles, 1790);
+            MadWave.AddConsignment(WomenSportSwimsuit.prodId, 1800, 20, WomenSportSwimsuit);
+            MadWave.AddConsignment(WomenBeachSwimsuit.prodId, 1500, 3, WomenBeachSwimsuit);
+            MadWave.AddConsignment(AdultPoolSlippers.prodId, 1200, 30, AdultPoolSlippers);
+            MadWave.AddConsignment(KidsPoolSlippers.prodId, 1000, 2, KidsPoolSlippers);
+            MadWave.AddConsignment(Towel.prodId, 500, 50, Towel);
+            
+            //fill Arena shop with goods
+            Arena.AddProduct(KidsGoggles, 700);
+            Arena.AddProduct(SwimmingTrunks, 1100);
+            Arena.AddConsignment(KidsPoolSlippers.prodId, 990, 40, KidsPoolSlippers);
+            Arena.AddConsignment(Towel.prodId, 600, 10, Towel);
+            
+            //Manager.cheapprodinfo cheapprodinfo = manager.FindCheapestProd(new Guid("73a303b4-c921-4f91-8c32-3231aa6c689a")); // finding the cheapest prod + filling in info 'bout it
+            Manager.cheapprodinfo cheapprodinfo = manager.FindCheapestProd(Towel.prodId); // finding the cheapest prod + filling in info 'bout it
             Console.WriteLine($"\nLowest price of {cheapprodinfo.ChProdName} is in a {cheapprodinfo.ChShopName} shop at {cheapprodinfo.ChShopAddress} and it costs {cheapprodinfo.ChProdPrice}");
 
             var sum = 3000; // declaring sum on which we wanna buy smth
-            var shopForList = shop1; // choosing a shop
+            var shopForList = MadWave; // choosing a shop
             
             shopForList.ProdListOnSum(sum); // getting the list of prod on exact sum
-            
-            Console.WriteLine($"\nList of amount of products you can buy in {shopForList.Name} on {sum}:");
-            foreach (var prod in shopForList.ProdList)
-            {
-                var amount = sum / prod.Value;
-                Console.WriteLine($"{prod.Key}: {amount} piece(-s) // {prod.Value} x {amount}");
-            }
 
             manager.ShowAvailabilityOfGoods();
             
-            Guid[] goodsForCons = {good1, good2}; // listing some of the prods we wanna buy 
-            int[] goodsAmountForCons = {15, 1}; // listing their amounts
-            var shopForCons = shop1; // choosing a shop
+            // buying consignment \/
+            Dictionary<Guid, int> GoodsForCons = new Dictionary<Guid, int>();
+            GoodsForCons.Add(MenSportSwimsuit.prodId, 1);
+            GoodsForCons.Add(Towel.prodId, 10);
             
-            var sumCons = manager.BuyConsignment(goodsForCons, goodsAmountForCons, shopForCons); // buying consignment
+            var shopForCons = Speedo; // choosing a shop
+            
+            var sumCons = manager.BuyConsignment(GoodsForCons, shopForCons); // buying consignment
             
             Console.WriteLine($"\nThe sum of your purchase: {sumCons}\n");
-            for(int i = 0; i < goodsForCons.Length; i++)
+            foreach (var good in GoodsForCons)
             {
-                var id = goodsForCons[i];
-                var goodSum = shopForCons.catalog[id].prodPrice * goodsAmountForCons[i];
-                Console.WriteLine($"{shopForCons.catalog[id].prodName}: {shopForCons.catalog[id].prodPrice} x {goodsAmountForCons[i]} = {goodSum}");
+                var id = good.Key;
+                var amount = good.Value;
+                var goodSum = shopForCons.catalog[id].prodPrice * amount;
+                Console.WriteLine($"{shopForCons.catalog[id].prodName}: {shopForCons.catalog[id].prodPrice} x {amount} = {goodSum}");
             }
 
             manager.ShowAvailabilityOfGoods();
-            Guid[] goodsForCheapestShop = {good1, good2}; // listing some of the goods we wanna buy at the lowest price
-            int[] goodsAmountForCheapestShop = {1, 1}; // listing their amounts
+            
+            // cheapest way to buy consignment \/
+            Dictionary<Guid, int> GoodsForCheapestShop = new Dictionary<Guid, int>();
+            GoodsForCheapestShop.Add(MenSportSwimsuit.prodId, 1);
+            GoodsForCheapestShop.Add(Towel.prodId, 1);
             var FinalCost = 0;
             
-            var CheapestShop = manager.FindCheapestShopToBuyCons(goodsForCheapestShop, goodsAmountForCheapestShop);
+            var CheapestShop = manager.FindCheapestShopToBuyCons(GoodsForCheapestShop);
             
             Console.WriteLine($"\nThe lowest sum of your purchase will be in a {CheapestShop.Name} shop at {CheapestShop.Address}." +
                               "\nYour list of the cheapest products:");
-            for (int i = 0; i < goodsForCheapestShop.Length; i++)
+            foreach (var good in GoodsForCheapestShop)
             {
-                var id = goodsForCheapestShop[i];
-                var goodSum = CheapestShop.catalog[id].prodPrice * goodsAmountForCheapestShop[i];
+                var id = good.Key;
+                var amount = good.Value;
+                
+                var goodSum = CheapestShop.catalog[id].prodPrice * amount;
                 FinalCost += goodSum;
-                Console.WriteLine($"{CheapestShop.catalog[id].prodName}: {CheapestShop.catalog[id].prodPrice} x {goodsAmountForCheapestShop[i]} = {goodSum}");
+                Console.WriteLine($"{CheapestShop.catalog[id].prodName}: {CheapestShop.catalog[id].prodPrice} x {amount} = {goodSum}");
             }
             Console.WriteLine($"\nFinal cost will be {FinalCost}");
         }
